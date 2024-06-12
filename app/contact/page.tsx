5e965f72bbde0683052e9eab312f6786b1feee6b
ariskaAdi <ariskaadiprasetyo@gmail.com>
2024-06-12 20:49:57 +0700

    search

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,14 +2,24 @@ import { CreateButton } from "@/components/button";
 import ContactTable from "@/components/contact-table";
 import Serach from "@/components/serach";
 
-const Contacts = () => {
+const Contacts = ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
   return (
     <div className="max-w-screen-md mx-auto mt-5">
       <div className="flex items-center justify-between gap-1 mb-5">
         <Serach />
         <CreateButton />
       </div>
-      <ContactTable />
+      <ContactTable query={query} currentPage={currentPage} />
     </div>
   );
 };
